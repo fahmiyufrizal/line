f2493dc38a3d3d819518135ba7783c150f4a5ec8
Fahmi Yufrizal Yusuf <60032856+fahmiyufrizal@users.noreply.github.com>
2024-02-05 20:43:29 +0700

    Add files via upload

---
+++ mini_scheduler_ayeye.py
@@ -0,0 +1,27 @@
+# LINE Mini-Updater Scheduler (Rebuilt)
+# by fahmiyufrizal@2022
+
+#import modules
+import os
+import configparser
+from os import path
+import subprocess
+from subprocess import Popen, PIPE
+import ctypes
+import _winapi
+
+#parameters
+version = ("[v1.0]")
+static_path = (r'C:\ProgramData\LINE-Autoschedule')
+dp0 = os.getcwd()
+
+os.system("title fahmiyufrizal@2023 [LINE-Mini-Updater Autoscheduler]")
+print(" ")
+print("*******************************************************************")
+print("*                    Auto-Scheduler Time!                         *")
+print("*              PS : Not A Virus, just doing things                *")
+print("*                                                                 *")
+print("*                Sorry for blinking command window                *")
+print("*                    S e e   y o u   s o o n !                    *")
+print("*                                                                 *")
+print("*******************************************************************")
